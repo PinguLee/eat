acbb3dc3eeb96621a92d311f83768ada5a6bba11
이민구 <escapeweedy@gmail.com>
2023-11-24 04:15:23 +0900

    feat: 로그인/회원가입 임시 완성

--- client.py
+++ client.py
@@ -1,49 +1,75 @@
-import socket
-import ast
+import sys
 
 def main():
-    while True:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(('127.0.0.1', 1313))
+    # 입력 받기
+    option = input("1. 로그인 2. 회원가입: ")
 
-        # 처음 실행 시 로그인 또는 회원가입 선택
-        print("1. 로그인\n2. 회원가입\n3. 종료")
-        choice = input("선택: ")
-        client.send(choice.encode())
+    # 로그인
+    if option == "1":
+        login()
 
-        if choice == '1':
-            # 로그인
-            print(client.recv(1024).decode(), end="")  # "Enter your user_id: "
-            user_id = input()
-            client.send(user_id.encode())
-            print(client.recv(1024).decode(), end="")  # "Enter your user_pw: "
-            user_pw = input()
+    # 회원가입
+    elif option == "2":
+        register()
 
-            print(f"[*] Sent user_id: {user_id}, user_pw: {user_pw}")
+    # 종료
+    else:
+        print("종료합니다.")
+        sys.exit()
 
-            login_data = {'user_id': user_id, 'user_pw': user_pw}
-            client.send(str(login_data).encode())
-            print(client.recv(1024).decode())  # Login result
+def login():
+    # ID 입력
+    id = input("ID를 입력하세요: ")
 
-        elif choice == '2':
-            # 회원가입
-            user_data = {
-                'name': input("Enter your name: "),
-                'age': input("Enter your age: "),
-                'school': input("Enter your school: "),
-                'department': input("Enter your department: "),
-                'intro': input("Enter your introduction: "),
-                'user_id': input("Enter your user_id: "),
-                'user_pw': input("Enter your user_pw: ")
-            }
-            client.send(str(user_data).encode())
-            print(client.recv(1024).decode())  # Registration result
+    # 데이터베이스에서 ID 검색
+    with open("database.txt", "r") as f:
+        for line in f:
+            if line.split("|")[0] == id:
+                # ID가 존재하면 PW 입력받기
+                password = input("PW를 입력하세요: ")
 
-        elif choice == '3':
-            print("프로그램을 종료합니다.")
-            break
+                # PW도 일치하면 로그인 성공
+                if line.split("|")[1] == password:
+                    print("로그인 성공!")
 
-        client.close()
+                    # 서버로부터 수신한 데이터 처리
+                    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    connection.connect(("localhost", 8080))
+                    data = connection.recv(1024)
+                    if data.decode("utf-8") == "로그인 성공!":
+                        name = data.decode("utf-8").split("|")[2]
+                        age = data.decode("utf-8").split("|")[3]
+                        school = data.decode("utf-8").split("|")[4]
+                        department = data.decode("utf-8").split("|")[5]
+                        self_introduction = data.decode("utf-8").split("|")[6]
+                        print(f"이름: {name}\n나이: {age}\n학교: {school}\n학과: {department}\n자기소개: {self_introduction}")
+                    return
+
+    # ID가 존재하지 않으면 로그인 실패
+    print("존재하지 않는 ID입니다.")
+
+def register():
+    # 회원 정보 입력
+    name = input("이름을 입력하세요: ")
+    age = input("나이를 입력하세요: ")
+    school = input("학교를 입력하세요: ")
+    department = input("학과를 입력하세요: ")
+    self_introduction = input("자기소개를 입력하세요: ")
+    id = input("ID를 입력하세요: ")
+    password = input("PW를 입력하세요: ")
+
+    # 데이터베이스에 회원 정보 전송
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.connect(("localhost", 8080))
+    connection.sendall(f"{name}|{age}|{school}|{department}|{self_introduction}|{id}|{password}".encode("utf-8"))
+
+    data = connection.recv(1024)
+
+    # 결과 출력
+    if data.decode("utf-8") == "회원가입 성공!":
+        print("회원가입 성공!")
+    else:
+        print(data.decode("utf-8"))
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,61 +1,79 @@
 import socket
-import threading
-import ast
-
-def register_user(data):
-    with open('database.txt', 'a') as f:
-        f.write(data + '\n')
-
-def login_user(user_id, user_pw):
-    with open('database.txt', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            user_data = ast.literal_eval(line.strip())
-            if user_data['user_id'] == user_id and user_data['user_pw'] == user_pw:
-                return True
-    return False
-
-def handle_client(client_socket):
-    # 처음 실행 시 로그인 또는 회원가입 선택
-    choice = client_socket.recv(1024).decode()
-# server.py
-    if choice == '1':
-        # 로그인
-        client_socket.send("Enter your user_id: ".encode())
-        user_id = client_socket.recv(1024).decode()
-        client_socket.send("Enter your user_pw: ".encode())
-        user_pw = client_socket.recv(1024).decode()
-
-        print(f"[*] Received user_id: {user_id}, user_pw: {user_pw}")
-
-        if login_user(user_id, user_pw):
-            print("Login successful!")
-            client_socket.send("Login successful!".encode())
-        else:
-            print("Login failed. Invalid user_id or user_pw.")
-            client_socket.send("Login failed. Invalid user_id or user_pw.".encode())
-
-    elif choice == '2':
-        # 회원가입
-        user_data_str = client_socket.recv(1024).decode()
-        user_data = ast.literal_eval(user_data_str)
-        register_user(str(user_data))
-        client_socket.send("Registration successful!".encode())
-
-    client_socket.close()
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('127.0.0.1', 1313))
-    server.listen(5)
+    # 서버 소켓 생성
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("localhost", 8080))
+    server_socket.listen(1)
 
-    print("[*] Listening for connections on 127.0.0.1:12345")
+    # 서버 실행 중임을 알림
+    print("서버 실행 중")
 
-    while True:
-        client_socket, addr = server.accept()
-        print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-        client_handler.start()
+    # 클라이언트 연결 대기
+    connection, address = server_socket.accept()
+
+    # 클라이언트로부터 데이터 수신
+    data = connection.recv(1024)
+
+    # 데이터 처리
+    option = data.decode("utf-8")
+
+    # 로그인
+    if option == "1":
+        login(connection)
+
+    # 회원가입
+    elif option == "2":
+        register(connection)
+
+    # 종료
+    else:
+        print("종료합니다.")
+        connection.close()
+        server_socket.close()
+        sys.exit()
+
+def login(connection):
+    # ID 입력
+    id = connection.recv(1024).decode("utf-8")
+
+    # 데이터베이스에서 ID 검색
+    with open("database.txt", "r") as f:
+        for line in f:
+            if line.split("|")[0] == id:
+                # ID가 존재하면 PW 입력받기
+                password = connection.recv(1024).decode("utf-8")
+
+                # PW도 일치하면 로그인 성공
+                if line.split("|")[1] == password:
+                    # 회원 정보 출력
+                    connection.sendall("로그인 성공!")
+                    name = line.split("|")[2]
+                    age = line.split("|")[3]
+                    school = line.split("|")[4]
+                    department = line.split("|")[5]
+                    self_introduction = line.split("|")[6]
+                    connection.sendall(f"{name}|{age}|{school}|{department}|{self_introduction}".encode("utf-8"))
+                    return
+
+    # ID가 존재하지 않으면 로그인 실패
+    connection.sendall("존재하지 않는 ID입니다.")
+
+def register(connection):
+    # 회원 정보 입력
+    name = connection.recv(1024).decode("utf-8")
+    age = connection.recv(1024).decode("utf-8")
+    school = connection.recv(1024).decode("utf-8")
+    department = connection.recv(1024).decode("utf-8")
+    self_introduction = connection.recv(1024).decode("utf-8")
+    id = connection.recv(1024).decode("utf-8")
+    password = connection.recv(1024).decode("utf-8")
+
+    # 데이터베이스에 회원 정보 저장
+    with open("database.txt", "a") as f:
+        f.write(f"{name}|{age}|{school}|{department}|{self_introduction}|{id}|{password}\n")
+
+    connection.sendall("회원가입 성공!")
 
 if __name__ == "__main__":
     main()
